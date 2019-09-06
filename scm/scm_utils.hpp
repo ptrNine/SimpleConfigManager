f1e3461e1066d2f7f3a9bb1d76350b86d5b79697
ptrNine <sockskilling@gmail.com>
2019-09-07 00:39:34 +0300

    Add documentation for 'scm_utils.hpp'

--- scm/scm_utils.hpp
+++ scm/scm_utils.hpp
@@ -18,30 +18,98 @@
 #endif
 
 namespace scm_utils {
+    /**
+     * Input file stream exception
+     */
+    class ScmIfsException : public std::exception {
+    public:
+        explicit ScmIfsException(std::string error) : _exc(std::move(error)) {}
+        const char* what() const noexcept override { return _exc.data(); }
+
+    private:
+        std::string _exc;
+    };
+
+    /**
+     * Config exception
+     */
+    class CfgException : public std::exception {
+    public:
+        explicit CfgException(std::string error) : _exc(std::move(error)) {}
+        const char* what() const noexcept override { return _exc.data(); }
+
+    private:
+        std::string _exc;
+    };
+
+    /**
+     * Filesystem exception
+     */
+    class ScmFsException : public std::exception {
+    public:
+        explicit ScmFsException(std::string error) : _exc(std::move(error)) {}
+        const char* what() const noexcept override { return _exc.data(); }
+
+    private:
+        std::string _exc;
+    };
+
+    /**
+     * Check if type 'T' belongs to one of types 'Types'
+     * @tparam T - checked type
+     * @tparam Types - all valid types
+     */
     template <typename T, typename ... Types>
     constexpr bool any_of = (std::is_same_v<T, Types> || ...);
 
+    /**
+     * Check if type 'T' belongs to floats
+     * @tparam T - checked type
+     */
     template <typename T>
     constexpr bool floats =
             std::is_floating_point_v<std::remove_const_t<std::remove_reference_t<T>>>;
 
+    /**
+     * Check if type 'T' belongs to integers
+     * @tparam T - checked type
+     */
     template <typename T>
     constexpr bool integers =
             std::is_integral_v<std::remove_const_t<std::remove_reference_t<T>>> && !std::is_same_v<T, bool>;
 
+    /**
+     * Check if type 'T' belongs to numbers
+     * @tparam T - checked type
+     */
     template <typename T>
     constexpr bool numbers = integers<T> || floats<T>;
 
+    /**
+     * Check if type 'T' is std::string, std::string_view, ScmString or StmStrView
+     * @tparam T - checked type
+     */
     template <typename T>
     constexpr bool is_string = any_of<T, ScmString, ScmStrView, std::string, std::string_view>;
 
 
-    // Helper function for combine strings
+    /**
+     * Combine all accepted strings
+     * @tparam ArgT - types of strings
+     * @param strs - strings
+     * @return New string
+     */
     template <typename ... ArgT>
     static auto str_join(const ArgT& ... strs) -> std::string {
         return (std::string(strs) + ...);
     }
 
+    /**
+     * Drop the child path from string
+     * @tparam S - string type
+     * @param str - string with path
+     * @return string with deleted child path
+     */
     template <typename S>
     inline auto parent_path(const S& str) -> std::enable_if_t<is_string<S>, ScmString>
     {
@@ -63,6 +131,14 @@ namespace scm_utils {
         return str.substr(0, size);
     }
 
+    /**
+     * Append path to string
+     * @tparam S - first string type (also type of returned string)
+     * @tparam S2 - second string type
+     * @param p1 - path
+     * @param str2 - appended path
+     * @return new path
+     */
     template <typename S, typename S2>
     inline auto append_path(const S& p1, const S2& str2) -> std::enable_if_t<is_string<S>, ScmString>
     {
@@ -79,35 +155,12 @@ namespace scm_utils {
         }
     }
 
-    // File reader exception
-    class ScmIfsException : public std::exception {
-    public:
-        explicit ScmIfsException(std::string error) : _exc(std::move(error)) {}
-        const char* what() const noexcept override { return _exc.data(); }
-
-    private:
-        std::string _exc;
-    };
-
-    class CfgException : public std::exception {
-    public:
-        explicit CfgException(std::string error) : _exc(std::move(error)) {}
-        const char* what() const noexcept override { return _exc.data(); }
-
-    private:
-        std::string _exc;
-    };
-
-    // Filesystem exception
-    class ScmFsException : public std::exception {
-    public:
-        explicit ScmFsException(std::string error) : _exc(std::move(error)) {}
-        const char* what() const noexcept override { return _exc.data(); }
-
-    private:
-        std::string _exc;
-    };
-
+    /**
+     * Read whole file to string (without buffers, be careful!)
+     * @tparam S - string type
+     * @param name - path to file
+     * @return A string contained whole file
+     */
     template <typename S>
     inline auto read_file_to_string(const S& name) -> std::enable_if_t<is_string<S>, ScmString>
     {
@@ -127,7 +180,14 @@ namespace scm_utils {
         return std::move(str);
     }
 
-    inline auto split_view(const ScmStrView& str, std::initializer_list<char> l, bool createNullStrs)
+    /**
+     * Split string around given delimiters
+     * @param str - a string to split
+     * @param l - delimiters
+     * @param createNullStrs - flag for creating null strings (drops them if false)
+     * @return A vector with string_view's
+     */
+    inline auto split_view(const ScmStrView& str, std::initializer_list<char> l, bool createNullStrs = false)
     -> ScmVector<ScmStrView>
     {
         ScmVector<ScmStrView> vec;
